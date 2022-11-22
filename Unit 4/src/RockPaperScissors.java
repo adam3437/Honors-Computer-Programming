363b18bee4cea33565eb220a0cdb77ce19f0b6ee
Spy_Gaming_TF2 <78375663+Spy-Gaming-TF2@users.noreply.github.com>
2022-11-22 20:07:39 +0000

    Edited Rock Paper Scissors Program

--- Unit 4/src/RockPaperScissors.java
+++ Unit 4/src/RockPaperScissors.java
@@ -8,64 +8,50 @@ public class RockPaperScissors
         int compInput = (int)(Math.random() * (3-1+1) + 1);
         String userChoice = " ";
         String opponent = " ";
-        if(userInput > 3)
-        {
-            JOptionPane.showMessageDialog(null,"I said 1, 2, or 3 dumbass");
-        }
-        if(userInput == 1)
-        {
-            userChoice = "Rock";
-        }
-        else if(userInput == 2)
-        {
-            userChoice = "Paper";
-        }
-        else if(userInput == 3)
-        {
-            userChoice = "Scissors";
-        }
-        else if(compInput == 1)
-        {
-        	opponent = "Rock";
-        }
-        else if(compInput == 2)
-        {
-        	opponent = "Paper";
-        }
-        else if(compInput == 3)
-        {
-            opponent = "Scissors";
-        }
-
+        String gamesWon = " ";
+        String gamesLost = " ";
+        if(userInput > 3){ JOptionPane.showMessageDialog(null,"I said 1, 2, or 3 dumbass"); }
+        if(userInput == 1){ userChoice = "Rock"; }
+        else if(userInput == 2){ userChoice = "Paper"; }
+        else if(userInput == 3){ userChoice = "Scissors"; }
+        else if(compInput == 1){ opponent = "Rock"; }
+        else if(compInput == 2){ opponent = "Paper"; }
+        else if(compInput == 3){ opponent = "Scissors"; }
         if(userInput == 1 && compInput == 2)
         {
             JOptionPane.showMessageDialog(null, "You picked " + userChoice + ". Your opponent picked paper.");
             JOptionPane.showMessageDialog(null, "You Loose!");
+            gamesLost = gamesLost + 1;
         }
         if(userInput == 1 && compInput == 3)
         {
             JOptionPane.showMessageDialog(null, "You picked " + userChoice + ". Your opponent picked scissors." );
             JOptionPane.showMessageDialog(null, "You Win!");
+            gamesWon = gamesWon + 1;
         }
         if(userInput == 2 && compInput == 1)
         {
             JOptionPane.showMessageDialog(null, "You picked " + userChoice + ". Your opponent picked rock.");
             JOptionPane.showMessageDialog(null, "You Win!");
+            gamesWon = gamesWon + 1;
         }
         if(userInput == 2 && compInput == 3)
         {
             JOptionPane.showMessageDialog(null, "You picked " + userChoice + ". Your opponent picked scissors.");
             JOptionPane.showMessageDialog(null, "You Loose!");
+            gamesLost = gamesLost + 1;
         }
         if(userInput == 3 && compInput == 1)
         {
             JOptionPane.showMessageDialog(null, "You picked " + userChoice + ". Your opponent picked rock.");
             JOptionPane.showMessageDialog(null, "You Loose!!");
+            gamesLost = gamesLost + 1;
         }
         if(userInput == 3 && compInput == 2)
         {
             JOptionPane.showMessageDialog(null, "You picked " + userChoice + ". Your opponent picked paper.");
             JOptionPane.showMessageDialog(null, "You Win!");
+            gamesWon = gamesWon + 1;
         }
         if(userInput == compInput)
         {
