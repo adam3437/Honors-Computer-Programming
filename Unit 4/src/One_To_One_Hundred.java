ffea02a9a2e01f6e57a949606df51f9939e3b75e
Spy_Gaming_TF2 <78375663+Spy-Gaming-TF2@users.noreply.github.com>
2022-11-28 20:03:47 +0000

    Edited One_To_One_Hundred Program

--- Unit 4/src/One_To_One_Hundred.java
+++ Unit 4/src/One_To_One_Hundred.java
@@ -1,9 +1,19 @@
-
-public class One_To_One_Hundred {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class One_To_One_Hundred
+ {
+	public static void main(String[] args)
+	 {
+		int x = 1;
+		while(x <= 100)
+		 {
+			if(x % 2 != 0)
+			 {
+				System.out.printf(x + " ");
+			 }
+			if(x % 20 == 0)
+			 {
+				System.out.println();
+			 }
+			x++;
+		 }
 	}
-
 }
