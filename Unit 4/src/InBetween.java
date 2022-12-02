994e1c3b6c65e84b725adb9df68badb156dd4530
Spy_Gaming_TF2 <78375663+Spy-Gaming-TF2@users.noreply.github.com>
2022-12-02 19:27:06 +0000

    Added In Between Program

---
+++ Unit 4/src/InBetween.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class InBetween 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter another integer: ");
+        int num2 = input.nextInt();
+        if (num1 < num2)
+        {
+            for (int i = num1 + 1; i < num2; i++)
+            {
+                System.out.print(i + " ");
+            }
+        }
+        else if (num1 > num2)
+        {
+            for (int i = num2 + 1; i < num1; i++)
+            {
+                System.out.print(i + " ");
+            }
+        }
+        else
+        {
+            System.out.println("There are no integers between the entered values.");
+        }
+        input.close();
+    }
+}
