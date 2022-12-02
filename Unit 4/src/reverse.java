b0eefbdaa9b8681319cef464ef89ba6122af4cf3
Spy_Gaming_TF2 <78375663+Spy-Gaming-TF2@users.noreply.github.com>
2022-12-02 18:38:30 +0000

    Added Reverse String Program

---
+++ Unit 4/src/reverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class reverse
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = input.nextLine();
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            System.out.print(str.charAt(i));
+        }
+        System.out.println(" ");
+        input.close();
+    }
+}
