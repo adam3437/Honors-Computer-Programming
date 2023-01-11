179b8f6302a08243421e4756784ab6760619f295
Spy_Gaming_TF2 <78375663+Spy-Gaming-TF2@users.noreply.github.com>
2023-01-11 19:09:16 +0000

    Updated Magic 8 Ball Program

--- Unit 4/src/Magic_8_Ball.java
+++ Unit 4/src/Magic_8_Ball.java
@@ -1,8 +1,5 @@
 import javax.swing.JOptionPane;
-/*
-    Magic 8 Ball
-    needs little explination
- */
+
 public class Magic_8_Ball 
 {
     public static void main(String[] args)
@@ -10,11 +7,9 @@ public class Magic_8_Ball
         boolean keepAsking = true;
         while (keepAsking)
         {
-            // random number generator
             int random = (int)(Math.random() * 5);
             String question = JOptionPane.showInputDialog("Ask a question");
             if (question == null){keepAsking = false;}
-            //response chosen based on random number that was generated earlier
             else if (random == 0){JOptionPane.showMessageDialog(null, "It is certain");}
             else if (random == 1){JOptionPane.showMessageDialog(null, "It is decidedly so");}
             else if (random == 2){JOptionPane.showMessageDialog(null, "Without a doubt");}
@@ -22,7 +17,6 @@ public class Magic_8_Ball
             else if (random == 4){JOptionPane.showMessageDialog(null, "Outlook not so good");}
             else if (random == 5){JOptionPane.showMessageDialog(null, "My sources say no");}
             else if (random == 6) {JOptionPane.showMessageDialog(null, "Don't count on it");}
-            // ask if they want to continue
             int cont = JOptionPane.showConfirmDialog(null, "Do you want to continue?");
             if(cont == 1) {keepAsking = false;}
         }
