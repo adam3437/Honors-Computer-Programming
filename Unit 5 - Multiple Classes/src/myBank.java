1a0ac2f69dc27e249c01fb0c4ad3c8d4b6056c17
Spy_Gaming_TF2 <78375663+Spy-Gaming-TF2@users.noreply.github.com>
2023-01-19 20:04:13 +0000

    Updated Piggy Bank Program
    Fixed multiple issues with the program

--- Unit 5 - Multiple Classes/src/myBank.java
+++ Unit 5 - Multiple Classes/src/myBank.java
@@ -2,25 +2,28 @@ import javax.swing.JOptionPane;
 /** 
  * This class creates a piggyBank object and uses the piggyBank class to get the total amount of money and coin count
  * @author Spy_Gaming_TF2
- * 1/12/2022
+ * 1/19/2022
  */
 public class myBank
 {
     public static void main(String[] args)
     {
-        piggyBank myBank = new piggyBank(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    	//Initializes the piggy bank
+        piggyBank myBank = new piggyBank(0, 0, 0, 0);
+        String numPennies = JOptionPane.showInputDialog("How many pennies do you have?");
+        if (numPennies == null) System.exit(0);
+        String numNickels = JOptionPane.showInputDialog("How many nickels do you have?");
+        if (numNickels == null) System.exit(0);
+        String numDimes = JOptionPane.showInputDialog("How many dimes do you have?");
+        if (numDimes == null) System.exit(0);
+        String numQuarters = JOptionPane.showInputDialog("How many quarters do you have?");
+        if (numQuarters == null) System.exit(0);
         //gets user input for each coin and bill
-        myBank.setPennies(Integer.parseInt(JOptionPane.showInputDialog("How many pennies do you have?")));
-        myBank.setNickels(Integer.parseInt(JOptionPane.showInputDialog("How many nickels do you have?")));
-        myBank.setDimes(Integer.parseInt(JOptionPane.showInputDialog("How many dimes do you have?")));
-        myBank.setQuarters(Integer.parseInt(JOptionPane.showInputDialog("How many quarters do you have?")));
-        myBank.setOnes(Integer.parseInt(JOptionPane.showInputDialog("How many ones do you have?")));
-        myBank.setFives(Integer.parseInt(JOptionPane.showInputDialog("How many fives do you have?")));
-        myBank.setTens(Integer.parseInt(JOptionPane.showInputDialog("How many tens do you have?")));
-        myBank.setTwenties(Integer.parseInt(JOptionPane.showInputDialog("How many twenties do you have?")));
-        myBank.setFifties(Integer.parseInt(JOptionPane.showInputDialog("How many fifties do you have?")));
-        myBank.setHundreds(Integer.parseInt(JOptionPane.showInputDialog("How many hundreds do you have?")));
+        myBank.setPennies(Integer.parseInt(numPennies));
+        myBank.setNickels(Integer.parseInt(numNickels));
+        myBank.setDimes(Integer.parseInt(numDimes));
+        myBank.setQuarters(Integer.parseInt(numQuarters));
         //prints out the total amount of money and coin count of the piggy bank    
-        System.out.println(myBank.toString());
+        System.out.println(myBank.contents());
     }
 }
--- Unit 5 - Multiple Classes/src/piggyBank.java
+++ Unit 5 - Multiple Classes/src/piggyBank.java
@@ -1,7 +1,7 @@
 /**
  * This class is a piggy bank that holds coins and bills
  * @author Spy_Gaming_TF2 
- * 1/12/2022
+ * 1/19/2022
  */
 
 public class piggyBank 
@@ -11,25 +11,13 @@ public class piggyBank
     private int nickels;
     private int dimes;
     private int quarters;
-    private int ones;
-    private int fives;
-    private int tens;
-    private int twenties;
-    private int fifties;
-    private int hundreds;
     //constructor to initialize instance variables
-    public piggyBank(int thePennies, int theNickels, int theDimes, int theQuarters, int theOnes, int theFives, int theTens, int theTwenties, int theFifties, int theHundreds)
+    public piggyBank(int thePennies, int theNickels, int theDimes, int theQuarters)
     {
         pennies = thePennies;
         nickels = theNickels;
         dimes = theDimes;
         quarters = theQuarters;
-        ones = theOnes;
-        fives = theFives;
-        tens = theTens;
-        twenties = theTwenties;
-        fifties = theFifties;
-        hundreds = theHundreds;
     }
     //define methods to get info about the object
     public int getPennies()
@@ -48,30 +36,6 @@ public class piggyBank
     {
         return quarters;
     }
-    public int getOnes()
-    {
-        return ones;
-    }
-    public int getFives()
-    {
-        return fives;
-    }
-    public int getTens()
-    {
-        return tens;
-    }
-    public int getTwenties()
-    {
-        return twenties;
-    }
-    public int getFifties()
-    {
-        return fifties;
-    }
-    public int getHundreds()
-    {
-        return hundreds;
-    } 
     //allows for values to be changed by user input
     public void setPennies(int thePennies)
     {
@@ -89,37 +53,13 @@ public class piggyBank
     {
         quarters = theQuarters;
     }
-    public void setOnes(int theOnes)
-    {
-        ones = theOnes;
-    }
-    public void setFives(int theFives)
-    {
-        fives = theFives;
-    }
-    public void setTens(int theTens)
-    {
-        tens = theTens;
-    }
-    public void setTwenties(int theTwenties)
-    {
-        twenties = theTwenties;
-    }
-    public void setFifties(int theFifties)
-    {
-        fifties = theFifties;
-    }
-    public void setHundreds(int theHundreds)
-    {
-        hundreds = theHundreds;
-    }
     //toString method to print out the contents of the piggy bank 
     public String contents()
     {
-        return "Pennies: " + pennies + "\tNickels: " + nickels + "\tDimes: " + dimes + "\tQuarters: " + quarters + "\tOnes: " + ones + "\tFives: " + fives + "\tTens: " + tens + "\tTwenties: " + twenties + "\tFifties: " + fifties + "\tHundreds: " + hundreds + "\tTotal Value: $" + totalValue() + "\n";
+        return "Pennies: " + pennies + "\nNickels: " + nickels + "\nDimes: " + dimes + "\nQuarters: " + quarters + "\nTotal Value: $" + totalValue() + "\n";
     }
     public double totalValue()
     {
-        return pennies * 0.01 + nickels * 0.05 + dimes * 0.10 + quarters * 0.25 + ones * 1.00 + fives * 5.00 + tens * 10.00 + twenties * 20.00 + fifties * 50.00 + hundreds * 100.00;
+        return pennies * 0.01 + nickels * 0.05 + dimes * 0.10 + quarters * 0.25;
     }
 }
