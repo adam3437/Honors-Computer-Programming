f4613ebc6ee2e9d2d357d0af5c95104ff6281561
Spy-Gaming-TF2 <adam37-dev@protonmail.com>
2023-02-02 09:25:16 -0700

    Edited Bank Account Program
    Fixed problem where balance wouldn't update and name would be null

--- Unit 5 - Multiple Classes/src/bankAccount.java
+++ Unit 5 - Multiple Classes/src/bankAccount.java
@@ -11,21 +11,21 @@ public class bankAccount
     private double withdraw;
     private double balance;
     
-    public bankAccount(String theName, double theBalance)
+    public bankAccount(String name, double balance)
     {
-        theName = name;
-        theBalance = balance;
+        this.name = name;
+        this.balance = balance;
     }
 
     //Getters
-    public void deposit(double theDeposit)
+    public void deposit(double deposit)
     {
-        theDeposit = deposit;
+        this.deposit = deposit;
         balance = balance + deposit;
     }
-    public void withdraw(double theWithdraw)
+    public void withdraw(double withdraw)
     {
-        theWithdraw = withdraw;
+        this.withdraw = withdraw;
         balance = balance - withdraw;
     }
     //Setters
