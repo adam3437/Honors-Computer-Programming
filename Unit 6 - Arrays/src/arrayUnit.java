67aa2cbbf40fbf3f02012efd1a3bd1e4721701b6
Spy-Gaming-TF2 <adam37-dev@protonmail.com>
2023-04-21 12:47:50 -0700

    Added arrayUnit Program

---
+++ Unit 6 - Arrays/src/arrayUnit.java
@@ -0,0 +1,31 @@
+/**
+ * Makes an array with one element for each possible outcome. Set each element to 0. Then keep calling the random number generator.
+ * If it returns a value v, then increment the counter belonging to v. Extended Learning: print out the % for each index.
+ * @author Spy-Gaming-TF2
+ * 4/19/2023
+ */
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+public class arrayUnit
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        //asks the user how many numbers they want to generate
+        System.out.print("How many numbers do you want to generate? ");
+        int num = scan.nextInt();
+        System.out.print("How long should the array be? ");
+        int length = scan.nextInt();
+        int[] arr = new int[length];
+        //picks random ints that were generated to store in the array
+        for (int i = 0; i < num; i++)
+        {
+            int random = rand.nextInt(length);
+            arr[random]++;
+        }
+        System.out.println(Arrays.toString(arr));
+        scan.close();
+    }
+}
