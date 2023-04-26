08421c083eb4635b58504e86a5e3ee8749b5efb8
Spy-Gaming-TF2 <adam37-dev@protonmail.com>
2023-04-26 09:05:46 -0700

    Edited Multiple Choice Quiz Program
    Reworded one of the questions to fit the answer better

--- Unit 6 - Arrays/src/multipleChoiceQuiz.java
+++ Unit 6 - Arrays/src/multipleChoiceQuiz.java
@@ -14,7 +14,7 @@ public class multipleChoiceQuiz
     {
         //parallel arrays for the question and answers
         String[] questions = {"What year was the Declaration of Independence signed?", "Who was the 40th President of the US?",
-                "What year was the \"Iron Curtain\" torn down?", "When was the PATRIOT Act passed?", "What is the youngest country?"};
+                "What year did the \"Iron Curtain\" start to fall?", "When was the PATRIOT Act passed?", "What is the youngest country?"};
         String[] aAns = {"1776", "Barack Obama", "1921", "February 2002", "Russian Federation"};
         String[] bAns = {"1863", "Richard Nixon", "1958", "October 2001", "United States"};
         String[] cAns = {"1791", "Ronald Regan", "1991", "June 2010", "Germany"};
